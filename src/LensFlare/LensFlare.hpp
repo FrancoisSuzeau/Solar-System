cc9eb942acff0f99943ed5e6f11f43799857521f
FrancoisSuzeau <frsuzeau@gmail.com>
2022-02-06 13:20:46 +0100

    Creating LensFlare class

---
+++ src/LensFlare/LensFlare.hpp
@@ -0,0 +1,38 @@
+/*
+AUTHOR : SUZEAU François
+
+DATE : 06/02/2022
+
+MODULE : LensFlare
+
+NAMEFILE : LensFlare.hpp
+
+PURPOSE : Interface LensFlare
+*/
+
+#ifndef LENSFLARE_H
+#define LENSFLARE_H
+
+
+/********************************************************************* includes *********************************************************************/
+
+        #include <iostream>
+
+/********************************************************************* class definition *********************************************************************/
+        
+        class LensFlare
+        {
+            private:
+            
+                
+                
+
+            public:
+
+                LensFlare();
+                ~LensFlare();
+            
+        };
+
+
+#endif
